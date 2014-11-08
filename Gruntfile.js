4b64223671ad8c5c02d69a776f57046b677f4d53
stavinski <stavinski_2000@yahoo.com>
2014-11-08 12:22:00 +0000

    Adding NOTES
    Adding jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,17 +3,24 @@
 var timeGrunt = require('time-grunt'),
     loadTasks = require('load-grunt-tasks');
 
+var jspaths = ['*.js', '!node_modules', '!build', '!dist'];
+
 module.exports = function (grunt) {
 
   // time grunt
   timeGrunt(grunt);
 
   grunt.initConfig({
-
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      files: jspaths
+    }
   });
 
   // load tasks
   loadTasks(grunt);
 
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['jshint']);
 };
