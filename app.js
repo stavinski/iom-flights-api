8a2c778a94d634b76bc1b0998efe760048b04d3f
stavinski <stavinski_2000@yahoo.com>
2014-11-25 19:08:15 +0000

    feat: Added logging
    Fixed caching with query parameters

--- app.js
+++ app.js
@@ -6,17 +6,31 @@ require('./globals');
 var express = require('express'),
     app = express(),
     cors = require('cors'),
+    logger = require('morgan'),
+    errorhandler = require('errorhandler'),
     routerV1 = express.Router(),
     routes = libRequire('routes');
 
+// we handle the caching based off the data feed
+app.set('etag', false);
+app.set('port', conf('PORT'));
+
 // allow cross origin sharing
 app.use(cors());
+
+// log requests
+app.use(logger('dev'));
 
 // setup routes based off version
 routes.v1(routerV1);
 app.use('/v1', routerV1);
 
-// we handle the caching based off the data feed
-app.set('etag', false);
+// use error handler only in dev
+if (process.env.NODE_ENV === 'development') {
+  // only use in development
+  app.use(errorhandler());
+}
 
-app.listen(conf('PORT'));
+app.listen(conf('PORT'), function () {
+  console.log('flights api listening on port: ' + app.get('port'));
+});
