3f49904ec6debb38e2847646c1b0d18687e66300
stavinski <stavinski_2000@yahoo.com>
2014-11-25 17:33:26 +0000

    feat: Added CORS support

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ require('./globals');
 
 var express = require('express'),
     app = express(),
+    cors = require('cors'),
     routerV1 = express.Router(),
     routes = libRequire('routes');
+
+// allow cross origin sharing
+app.use(cors());
 
 // setup routes based off version
 routes.v1(routerV1);
 app.use('/v1', routerV1);
 
+// we handle the caching based off the data feed
 app.set('etag', false);
 
 app.listen(conf('PORT'));
