12971c2206117f783aef28c9df991bf06e1c7b8b
stavinski <stavinski_2000@yahoo.com>
2015-03-27 22:19:07 +0000

    feat: adding date formatting

---
+++ test/specs/dates.js
@@ -0,0 +1,27 @@
+'use strict';
+
+//load globals
+require('../../globals');
+
+let expect = require('expect.js'),
+    dates = libRequire('dates');
+
+describe('when formatting a date to local ISO string', function () {
+
+  beforeEach(() => dates());
+
+  it('should handle a normal GMT datetime', function () {
+    let val = new Date('01 Nov 2014 10:00:00'),
+        result = val.toLocalISOString();
+
+    expect(result).to.be('2014-11-01T10:00:00');
+  });
+
+  it('should handle a DST datetime', function () {
+    var val = new Date('01 Aug 2014 10:00:00'),
+        result = val.toLocalISOString();
+
+    expect(result).to.be('2014-08-01T10:00:00');
+  });
+
+});
