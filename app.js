7862d11fdb7201d6b6e11188d3b742056974843f
stavinski <stavinski_2000@yahoo.com>
2015-03-27 23:52:48 +0000

    feat: adding cache check for requests

--- app.js
+++ app.js
@@ -3,22 +3,25 @@
 // load globals
 require('./globals');
 
-var util = require('util'),
+let util = require('util'),
     app = require('koa')(),
     koalogger = require('koa-logger'),
     router = require('koa-router')(),
     cors = require('koa-cors'),
     jsonp = require('koa-jsonp');
 
+// middlewares
+let cachecheck = libRequire('cachecheck');
+
 // controllers
-var caching = libRequire('controllers/caching'),
+let caching = libRequire('controllers/caching'),
     flights = libRequire('controllers/flights')({});
 
 router.get('/v1/caching', caching.index);
 
 router.get('/v1/flights', flights.index)
-      .get('/v1/flights/arrivals', flights.arrivals)
-      .get('/v1/flights/departures', flights.departures);
+      .get('/v1/flights/arrivals', flights.arrivals, cachecheck)
+      .get('/v1/flights/departures', flights.departures, cachecheck);
 
 app.use(koalogger());
 app.use(cors());
